56d2d84a42888c9fb9db89003e48724702b6a9fc
Carter C <cclements3150@gmail.com>
2020-06-14 15:15:22 -0400

    Added insertion sort stuff to header

--- sort.h
+++ sort.h
@@ -15,8 +15,14 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+/* Sorting algorithms */
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+
+/* Helper functions/tools */
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
+listint_t *create_listint(const int *array, size_t size);
+void swap_nodes(listint_t *left, listint_t *right);
 
 #endif
